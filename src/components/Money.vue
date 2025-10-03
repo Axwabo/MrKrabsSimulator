fd58b117adb3cdcece6fe9d461dc5d1a9041ca8d
Axwabo <axwabo@gmail.com>
2025-10-03 10:29:11 +0200

    money click audio

--- src/components/Money.vue
+++ src/components/Money.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 import useGameStore from "../gameStore.ts";
+import playMoney from "../money.ts";
 
 const { money } = storeToRefs(useGameStore());
 </script>
 
 <template>
-    <span id="money">💲 {{ money }}</span>
+    <span id="money" v-on:click="playMoney()">💲 {{ money }}</span>
 </template>
 
 <style scoped>
 #money {
     position: absolute;
-    top: 2rem;
-    left: 2rem;
+    top: 1rem;
+    left: 1rem;
+    padding: 1rem;
     font-size: 2rem;
     font-weight: bold;
     -webkit-text-stroke: 0.05em black;
+    user-select: none;
 }
 </style>
