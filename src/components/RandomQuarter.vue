802f1021599c76e7b0bb70ddd420fd35099588cc
Axwabo <axwabo@gmail.com>
2025-10-04 10:20:44 +0200

    bigger coin + fix roll

--- src/components/RandomQuarter.vue
+++ src/components/RandomQuarter.vue
@@ -1,52 +1,55 @@
 <script setup lang="ts">
 import coin from "/images/coin.webp";
 import useGameStore from "../gameStore.ts";
-import { computed, nextTick, onMounted, ref } from "vue";
+import { onMounted, ref } from "vue";
 
 const { earn } = useGameStore();
 
-const from = ref(window.innerWidth * 2);
-const to = ref(0);
+const element = ref<HTMLDivElement>();
 
-const fromPx = computed(() => `${from.value}px`);
-const toPx = computed(() => `${to.value}px`);
-
-const speed = ref("1s");
+const direction = ref<PlaybackDirection>("normal");
 
 function collectCoin() {
     earn(0.25);
-    nextTick(rollNext);
+    rollNext();
 }
 
-function rollNext(applySpeed: boolean = true) {
-    const offset = window.innerWidth * (Math.random() * 10 + 1);
-    if (applySpeed)
-        speed.value = `${Math.abs(from.value - offset) / 1000}s`;
-    from.value = to.value;
-    to.value = offset * Math.sign(Math.random() - 0.5);
+function rollNext() {
+    const div = element.value!;
+    div.getAnimations()[0]?.cancel();
+    direction.value = Math.random() < 0.5 ? "reverse" : "normal";
+    div.animate([
+        { visibility: "visible", translate: "0 0", left: "-2rem" },
+        { visibility: "visible", translate: "100vw 0", left: "2rem" }
+    ], {
+        duration: window.innerWidth * 3,
+        delay: Math.random() * 10000 + 10000,
+        direction: direction.value
+    }).addEventListener("finish", rollNext);
 }
 
-onMounted(() => rollNext(false));
+onMounted(rollNext);
 </script>
 
 <template>
-    <div class="quarter" v-on:animationend="rollNext()">
-        <img :src="coin" alt="" v-on:click="collectCoin()">
+    <div ref="element" class="quarter">
+        <img :src="coin" alt="" v-on:click="collectCoin()" draggable="false">
     </div>
 </template>
 
 <style scoped>
 .quarter {
     position: absolute;
-    left: 50%;
     bottom: 0;
-    animation: move v-bind(speed) linear;
+    visibility: hidden;
 }
 
 .quarter img {
-    max-width: 1rem;
+    max-width: 2rem;
     animation: roll 1s linear infinite;
+    animation-direction: v-bind(direction);
     user-select: none;
+    -webkit-user-drag: none;
 }
 
 @keyframes roll {
@@ -55,15 +58,6 @@ onMounted(() => rollNext(false));
     }
     to {
         rotate: 1turn;
-    }
-}
-
-@keyframes move {
-    from {
-        translate: v-bind(fromPx) 0;
-    }
-    to {
-        translate: v-bind(toPx) 0;
     }
 }
 </style>
