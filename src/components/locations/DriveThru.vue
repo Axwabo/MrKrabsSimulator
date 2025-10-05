07c8ea945de0aa57c235a4dbb10097268b748935
Axwabo <axwabo@gmail.com>
2025-10-05 09:24:05 +0200

    drive thru auto trigger

--- src/components/locations/DriveThru.vue
+++ src/components/locations/DriveThru.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
 import Navigator from "./Navigator.vue";
+import { storeToRefs } from "pinia";
+import useGameStore from "../../gameStore.ts";
+import { watch } from "vue";
+import useDialogStore from "../../dialogs/dialogStore.ts";
+
+const { location } = storeToRefs(useGameStore());
+
+const { interact } = useDialogStore();
+
+watch(location, value => {
+    if (value === "Drive Thru")
+        interact("Drive Thru");
+});
 </script>
 
 <template>
